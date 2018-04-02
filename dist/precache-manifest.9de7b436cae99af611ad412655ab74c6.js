cca4d305687f205587e9461592f13f26dd9aaf75
DanielSecco <33034487+DanielSecco@users.noreply.github.com>
2018-04-02 16:37:56 -0300

    Initial commit

---
+++ dist/precache-manifest.9de7b436cae99af611ad412655ab74c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "017ced36d82bea1e08b08393361e354d",
+    "url": "/src/js/idb.js"
+  },
+  {
+    "revision": "cb94c3608146875801c35b8e4ef2d36a",
+    "url": "/src/js/utility.min.js"
+  },
+  {
+    "revision": "c8bd728048f3f43ad288ffc84d13a57d",
+    "url": "/src/js/idb.min.js"
+  },
+  {
+    "revision": "b88c398a19b26ec091e5",
+    "url": "/js/vendor.b88c398a.js"
+  },
+  {
+    "revision": "4853a87685922cdf08b8bc5ed3f6f85e",
+    "url": "/src/js/utility.js"
+  },
+  {
+    "revision": "6b82fbb55ae19be4935964ae8c338e92",
+    "url": "/src/js/fetch.js"
+  },
+  {
+    "revision": "8e4af7513729aab7f021bef839d0fd6d",
+    "url": "/src/js/fetch.min.js"
+  },
+  {
+    "revision": "b25b0687e188f1777a154363d093e816",
+    "url": "/src/js/promise.min.js"
+  },
+  {
+    "revision": "10c2238dcd105eb23f703ee53067417f",
+    "url": "/src/js/promise.js"
+  },
+  {
+    "revision": "713af0c6ce93dbbce2f00bf0a98d0541",
+    "url": "/src/js/material.min.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "c74e0c846ee54a8479f3",
+    "url": "/js/app.c74e0c84.js"
+  },
+  {
+    "revision": "4db9b7232676821827736ab1d78721d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c74e0c846ee54a8479f3",
+    "url": "/css/app.81854de2.css"
+  }
+];
---
+++ src/components/comps/Foto.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="foto">
+    <h1>{{ msg }}</h1>
+      <div v-if="!image">
+    <input id="inputF" type="file" accept="video/*;capture=camcorder" @change="onFileChange">
+  </div>
+  <div v-else>
+    <img id="imgLoad" :src="image" width="100px" height="100px"/>
+      <input id="inputF" type="file" accept="video/*;capture=camcorder" @change="onFileChange">
+   <button @click="removeImage">Remove image</button>
+  </div>
+     <div id="lastIMG"> 
+    </div>
+  </div>
+</template>
+<script>
+    //var firebase = require('firebase');
+    var $ = require('jquery')
+    export default {
+        name: 'foto',
+        data() {
+            return {
+                msg: 'Escolher Arquivo',
+                image: '',
+                imageB: '',
+                urr: ''
+            }
+        },
+        methods: {
+            onFileChange(e) {
+                var files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                this.createImage(files[0]);
+                var file = e.target.files[0];
+                //var storageRef = firebase.storage().ref(file.name);
+                console.log('<filename >', file);
+                var metadata = {
+                    contentType: 'image/jpeg'
+                };
+            },
+            createImage(file) {
+                var image = new Image();
+                var reader = new FileReader();
+                var vm = this;
+                reader.onload = (e) => {
+                    vm.image = e.target.result;
+                    $("#lastIMG").append('<img  src="' + vm.image + '" width="100px" height="auto"/>');
+                    var ind = new Date().toISOString();
+                    var post = {
+                        id: ind,
+                        image: vm.image,
+                        fileName: file.name,
+                        user: 5585558
+                    };
+                    console.log('variavel>', post);
+                    writeData('posts', post);
+                    if ('indexedDB' in window) {
+                        readAllData('posts')
+                            .then(function(data) {
+                                console.log('From cache', data);
+                            });
+                    };
+                    fetch('http://192.168.0.8/lambanca', {
+                            method: 'POST',
+                            headers: {
+                                // 'dataType': 'json',
+                                'Access-Control-Allow-Origin': '*/*',
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json'
+                            },
+                            dataType: 'json',
+                            mode: 'cors',
+                            body: JSON.stringify([post])
+                        }).then(function(response) {
+                            console.log(response);
+                            return response.json();
+                        }).then(function (data) {
+                        
+                        console.log(data);
+                    })
+                        .catch(function(err) {
+                            console.log('******');
+                        });
+                    $("#imgLoad").remove();
+                };
+                reader.readAsDataURL(file);
+                console.log('readerfile > ', reader);
+                // use the Blob or File API
+                /*storageRef.put(reader).then(function(snapshot) {
+                	console.log('Uploaded a blob or file!');
+                });*/
+                //console.log(image.innerText + '<kkkkk>');
+            },
+            removeImage: function(e) {
+                $("#lastIMG").remove();
+            },
+        }
+    };
+    readAllData('posts')
+        .then(function(data) {
+            for (var dt of data) {
+                $("#lastIMG").append('<img  src="' + dt.image + '" width="100px" height="auto"/>');
+                $("#imgf").append('<img  src="' + dt.image + '" width="100px" height="auto"/>');
+            }
+        })
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1,
+    h2,
+    p {
+        font-weight: normal;
+        color: beige;
+    }
+
+</style>
